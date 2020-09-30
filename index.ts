3656ae94aecdac2ee71389cfed7a5afd226044b0
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2020-09-29 23:53:16 -0400

    improved build and serve commands

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const app = express()
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.use(express.static(path.join(__dirname, "..", "server/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 io.on('connection', (socket: any) => {
   console.log('a user connected');
@@ -15,5 +15,5 @@ http.listen(3001, () => {
 });
 
 app.use((req:any, res:any, next:any) => {
-  res.sendFile(path.join(__dirname, "..", "server/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
